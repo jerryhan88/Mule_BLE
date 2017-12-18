6292bb0e14581e8f8b8d15f121f30d359e9a5665
ckhan <chungkyun.han@gmail.com>
2017-12-18 12:53:50 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-for i in {0..4}; do
-    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(2, 'Lv2', $i)" &
-done
-
-for i in {0..4}; do
-    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(3, 'Lv2', $i)" &
-done
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(2, 'Lv2', $i)" &
+#done
+#
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(3, 'Lv2', $i)" &
+#done
 
 
 #for i in {0..4}; do
@@ -18,11 +18,11 @@ done
 #done
 
 
-#for i in {0..4}; do
-#    for j in {9..17}; do
-#        python3 -c "from dataProcessing import get_mTraj; get_mTraj('Lv4', $i, $j)" &
-#    done
-#done
+for i in {0..4}; do
+    for j in {9..17}; do
+        python3 -c "from dataProcessing import get_p_kmbl; get_p_kmbl('Lv4', $i, $j)" &
+    done
+done
 
 
 #python3 -c "from dataProcessing import arrange_M3_muleTraj; arrange_M3_muleTraj('Lv2')" &
