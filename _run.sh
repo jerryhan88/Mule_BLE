6c6ce53feabd24b3233de3b60602b732592f69eb
ckhan <chungkyun.han@gmail.com>
2017-12-18 12:55:17 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -9,20 +9,20 @@
 #done
 
 
-#for i in {0..4}; do
-#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv4', $i)" &
-#done
-#
-#for i in {0..4}; do
-#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv4', $i)" &
-#done
-
+for i in {0..4}; do
+    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv2', $i)" &
+done
 
 for i in {0..4}; do
-    for j in {9..17}; do
-        python3 -c "from dataProcessing import get_p_kmbl; get_p_kmbl('Lv4', $i, $j)" &
-    done
+    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv2', $i)" &
 done
+
+
+#for i in {0..4}; do
+#    for j in {9..17}; do
+#        python3 -c "from dataProcessing import get_p_kmbl; get_p_kmbl('Lv4', $i, $j)" &
+#    done
+#done
 
 
 #python3 -c "from dataProcessing import arrange_M3_muleTraj; arrange_M3_muleTraj('Lv2')" &
