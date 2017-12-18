68bc09266e722f9c3ab07fbdb5e2111042118502
ckhan <chungkyun.han@gmail.com>
2017-12-18 12:41:20 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -9,13 +9,15 @@
 #done
 
 
-for i in {0..4}; do
-    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv4', $i)" &
-done
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv4', $i)" &
+#done
+#
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv4', $i)" &
+#done
 
-for i in {0..4}; do
-    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv4', $i)" &
-done
+
 
 #for i in {0..4}; do
 #    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv2', $i)" &
@@ -27,11 +29,11 @@ done
 #done
 #
 
-#for i in {0..4}; do
-#    for j in {9..17}; do
-#        python3 -c "from dataProcessing import get_mTraj; get_mTraj('Lv2', $i, $j)" &
-#    done
-#done
+for i in {0..4}; do
+    for j in {9..17}; do
+        python3 -c "from dataProcessing import get_mTraj; get_mTraj('Lv2', $i, $j)" &
+    done
+done
 
 
 #python3 -c "from dataProcessing import arrange_M3_muleTraj; arrange_M3_muleTraj('Lv2')" &
