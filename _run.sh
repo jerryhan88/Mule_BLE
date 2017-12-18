578ab837154ea2ba2920f1c0cbe7c7bf4d72e559
ckhan <chungkyun.han@gmail.com>
2017-12-18 11:07:54 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(2, 'Lv4', $i)" &
+#done
+#
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(3, 'Lv4', $i)" &
+#done
+
+
 for i in {0..4}; do
-    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(2, 'Lv4', $i)" &
+    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv4', $i)" &
 done
 
 for i in {0..4}; do
-    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(3, 'Lv4', $i)" &
+    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv4', $i)" &
 done
 
 #for i in {0..4}; do
