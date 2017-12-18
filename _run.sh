034e3df82a7de003b58c36b8961237b0c10084a1
ckhan <chungkyun.han@gmail.com>
2017-12-18 09:07:39 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-for i in {0..4}; do
-    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv2', $i)" &
-done
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv2', $i)" &
+#done
+#
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv4', $i)" &
+#done
+
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv2', $i)" &
+#done
+#
+#for i in {0..4}; do
+#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv4', $i)" &
+#done
 
 for i in {0..4}; do
-    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv2', $i)" &
-done
-
-for i in {0..4}; do
-    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv4', $i)" &
-done
-
-for i in {0..4}; do
-    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv4', $i)" &
+    for j in {9..17}; do
+        python3 -c "from dataProcessing import get_p_kmbl; get_p_kmbl('Lv4', $i, $j)" &
+    done
 done
