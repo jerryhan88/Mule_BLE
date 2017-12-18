19682ee1364d435a58f3d2a62cf02a1d908dc369
ckhan <chungkyun.han@gmail.com>
2017-12-18 09:12:32 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -18,6 +18,6 @@
 
 for i in {0..4}; do
     for j in {9..17}; do
-        python3 -c "from dataProcessing import get_p_kmbl; get_p_kmbl('Lv4', $i, $j)" &
+        python3 -c "from dataProcessing import get_p_kmbl; get_p_kmbl('Lv2', $i, $j)" &
     done
 done
