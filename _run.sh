c54d49004f470143bc9a56e9345d4c1778d5d1ac
ckhan <chungkyun.han@gmail.com>
2018-04-17 08:37:59 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/env bash
 
-#for i in {0..4}; do
-#    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(2, 'Lv2', $i)" &
-#done
-#
-#for i in {0..4}; do
-#    python3 -c "from dataProcessing import gen_indiTrajectory; gen_indiTrajectory(3, 'Lv2', $i)" &
-#done
 
-#for i in {0..4}; do
-#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(2, 'Lv2', $i)" &
-#done
-#
-#for i in {0..4}; do
-#    python3 -c "from dataProcessing import aggregate_indiTrajectory; aggregate_indiTrajectory(3, 'Lv2', $i)" &
-#done
-
-#for i in {0..4}; do
-#    for j in {9..17}; do
-#        python3 -c "from dataProcessing import get_p_kmbl; get_p_kmbl('Lv2', $i, $j)" &
-#    done
-#done
-
-
-#python3 -c "from dataProcessing import arrange_M3_muleTraj; arrange_M3_muleTraj('Lv2')" &
-
-#python3 -c "from dataProcessing import arrange_M3_muleTraj; arrange_M3_muleTraj('Lv4')" &
-
-
-for repeatNum in {21..30}; do
-    python3 -c "from experiments import run_experiments_MA; run_experiments_MA($repeatNum, 'Lv4', N_g=50, N_p=100, N_o=80, p_c=0.5, p_m=0.5)" &
+for repeatNum in {1..30}; do
+    python3 -c "from experiments import run_experiments_MA; run_experiments_MA($repeatNum, 'Lv4', N_g=50, N_p=50, N_o=40, p_c=0.5, p_m=0.5)" &
 done
