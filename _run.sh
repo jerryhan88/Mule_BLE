773c6d2a70a05e96739eb11580eb9047e59eb2f7
ckhan <chungkyun.han@gmail.com>
2018-04-25 18:03:06 +0800

    temp

--- _run.sh
+++ _run.sh
@@ -2,5 +2,5 @@
 
 
 for repeatNum in {1..30}; do
-    python3 -c "from experiments import run_experiments_MA; run_experiments_MA($repeatNum, 'Lv2', N_g=50, N_p=100, N_o=80, p_c=0.5, p_m=0.5)" &
+    python3 -c "from experiments import run_experiments_MA; run_experiments_MA($repeatNum, 'Lv4', N_g=50, N_p=100, N_o=80, p_c=0.5, p_m=0.5)" &
 done
